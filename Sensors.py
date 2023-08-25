495c04e7c02fc9e7a439ddef9d3fd076be98fc49
krens22 <102179913+krens22@users.noreply.github.com>
2023-08-25 12:49:12 -0400

    Create Sensors.py

---
+++ Sensors.py
@@ -0,0 +1,10 @@
+from airflow.providors.http.sensors.http import HttpSensor
+
+#Adding a sensor to check if A URL is available or not
+
+is_api_available = HttpSensor(
+        task_id = 'is_api_available',
+        http_con_id = 'user_api',
+        endpoint = 'api/',
+        method='GET'
+)
