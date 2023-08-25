c6e19405666c9a861f9ba007f1f38d2eb5c629db
krens22 <102179913+krens22@users.noreply.github.com>
2023-08-25 13:04:27 -0400

    Update Sensors.py

--- Sensors.py
+++ Sensors.py
@@ -7,13 +7,3 @@ is_api_available = HttpSensor(
         endpoint = 'api/',
         method='GET'
 )
-
-#Extract the user data from the API
-extract_user = SimpleHttpOperator(
-        task_id = 'extract_user',
-        http_con_id = 'user_api',
-        endpoint = 'api/',
-        method='GET',
-        response_filter=lambda response: json.loads(response.text), #Extract the data and transform it in a Json format
-        log_response = True
-    )
