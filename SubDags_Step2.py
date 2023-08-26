665e1aae15cec9240862bda7e424fe30cf139cba
krens22 <102179913+krens22@users.noreply.github.com>
2023-08-25 22:44:45 -0400

    Update SubDags_Step2.py

--- SubDags_Step2.py
+++ SubDags_Step2.py
@@ -1,3 +1,8 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
+
+ with DAG (f"{parent_dag_id}.{child_dag_id}", 
+        start_date = datetime(2013,8,25),
+        schedule_intervals=@daily,
+        catchup = False) as dag:
