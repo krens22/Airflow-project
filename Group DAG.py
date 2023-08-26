e62dce93410dc94225f328cda48ba31a25211448
krens22 <102179913+krens22@users.noreply.github.com>
2023-08-26 11:57:42 -0400

    Update Group DAG.py

--- Group DAG.py
+++ Group DAG.py
@@ -2,13 +2,15 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
 from subdags.subdag_downloads import subdag_downloads
+from subdags.subdag_transforms import subdag_transforms
 from datetime import datetime
 
 with DAG('group_dag', start_date=datetime(2023, 8, 26), 
     schedule_interval='@daily', catchup=False) as dag:
     args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup':dag.catchup}
 
-# All the tasks get replaced with the subdag operator
+# All the tasks get replaced with the subdag operator for download tasks
+
     downloads = SubDagOperator(
     task_id='donwloads',
     subdag=subdag_downloads(dag.dag_id,'downloads',args))
